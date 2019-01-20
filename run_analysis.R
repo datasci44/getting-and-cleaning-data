18596f92e5c3ceb027283a6f7583cadaa817fdb4
Christopher Petr <christopher.k.petr@gmail.com>
2019-01-20 01:24:10 +0000

    Add sections & comments. Limit to single output. Change output format as per assignment instructions.

--- run_analysis.R
+++ run_analysis.R
@@ -3,11 +3,14 @@
 # Programming Assignment: Course 3 Week 4
 # Getting and Cleaning Data
 
-library(tidyverse)
 
+# Setup working environment ----------------
+
+library(tidyverse)
 setwd("/cloud/project")
 
-# Load activity labels & features
+
+# Load activity labels & features -----------------
 
 activity_labels <- readr::read_delim(file = "raw_files/UCI HAR Dataset/activity_labels.txt",
                                      delim = " ", 
@@ -21,19 +24,21 @@ features <- readr::read_delim(file = "raw_files/UCI HAR Dataset/features.txt",
   rename(index = X1,
          feature_name = X2)
 
-# Index columns with features wanted: mean() and std()
+
+# Index columns with wanted features: mean() and std() ----------
 
 features_wanted <- grep("(mean|std)\\(\\)", 
                         features$feature_name)
 
-# Load training data set
+
+# Load training data set ---------------
 
 train <- read_table(file = "raw_files/UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
 
-colnames(train) <- features$feature_name
+colnames(train) <- features$feature_name  # add columns names from features.txt
 
-train <- train[, features_wanted] %>%
-  janitor::clean_names(case = "snake")
+train <- train[, features_wanted] %>%  # filter columns to wanted features
+  janitor::clean_names(case = "snake")  # clean column names
 
 train_labels <- read_table(file = "raw_files/UCI HAR Dataset/train/y_train.txt", col_names = FALSE) %>%
   rename(label = X1)  
@@ -41,20 +46,20 @@ train_labels <- read_table(file = "raw_files/UCI HAR Dataset/train/y_train.txt",
 train_subjects <- read_table(file = "raw_files/UCI HAR Dataset/train/subject_train.txt", col_names = FALSE) %>%
   rename(subject = X1)
 
-train <- bind_cols(train, train_labels, train_subjects)
+train <- bind_cols(train, train_labels, train_subjects)  # merge 3 training data sources
 
-rm(train_labels)
+rm(train_labels)  # remove stale files
 rm(train_subjects)
 
 
-# Load testing data set
+# Load testing data set ---------------
 
 test <- read_table(file = "raw_files/UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
 
-colnames(test) <- features$feature_name
+colnames(test) <- features$feature_name  # add columns names from features.txt
 
-test <- test[, features_wanted] %>%
-  janitor::clean_names(case = "snake")
+test <- test[, features_wanted] %>%  # filter columns to wanted features
+  janitor::clean_names(case = "snake")  # clean column names
 
 test_labels <- read_table(file = "raw_files/UCI HAR Dataset/test/y_test.txt", col_names = FALSE) %>%
   rename(label = X1)  
@@ -62,33 +67,35 @@ test_labels <- read_table(file = "raw_files/UCI HAR Dataset/test/y_test.txt", co
 test_subjects <- read_table(file = "raw_files/UCI HAR Dataset/test/subject_test.txt", col_names = FALSE) %>%
   rename(subject = X1)
 
-test <- bind_cols(test, test_labels, test_subjects)
+test <- bind_cols(test, test_labels, test_subjects)  # merge 3 test data sources
 
-rm(test_labels)
+rm(test_labels)  # remove stale files
 rm(test_subjects)
 rm(features)
 rm(features_wanted)
 
-# Merge Training and Test Datasets
+
+# Merge Training and Test Datasets ---------------
 
 train_test <- bind_rows(train, test)
 
-rm(train)
+rm(train)  # remove stale files
 rm(test)
 
-# Add activity name associated with label
+
+# Add activity name associated with label ----------------
 
 train_test <- left_join(train_test, activity_labels, by = c("label" = "class_label")) %>%
   select(-label)
 
-rm(activity_labels)
+rm(activity_labels)  # remove stale file
 
-#glimpse(train_test)
+# glimpse(train_test)
 
-#write_csv(train_test, "train_test_tidy.csv")
+# write_csv(train_test, "train_test_tidy.csv")  # option to write full train & test data to csv
 
 
-# Average of each variable for each activity and each subject
+# Question 5: Average of each variable for each activity and each subject ----------------
 
 means <- train_test %>%
   group_by(activity_name, subject) %>%
@@ -96,4 +103,6 @@ means <- train_test %>%
 
 # View(means)
 
-# write_csv(means, "train_test_averages_tidy.csv")
+write.table(means, "train_test_averages.txt", row.names = FALSE)  # write means to text file as specified in assignment instructions
+
+# write_csv(means, "train_test_averages_tidy.csv")  # tidyverse version of csv write
